b45be752aa56b758ac5fc78f678a342b875a62ac
developer-abdulali <ab.ali.soomro@gmail.com>
2023-05-26 19:27:18 +0500

    added footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,87 +2,85 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../public/images/Panaverse.png";
 import Link from "next/link";
+import { FaFacebookF } from "react-icons/fa";
+import { FiYoutube } from "react-icons/fi";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
-      <footer className="bg-white dark:bg-gray-900">
-        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div className="md:flex md:justify-between">
-            <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
-                <Image
-                  src={logo}
-                  className=""
-                  alt="FlowBite Logo"
-                />
-              </a>
-              <p></p>
-            </div>
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Resources
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="https://flowbite.com/" className="hover:underline">
-                      Flowbite
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://tailwindcss.com/"
-                      className="hover:underline"
-                    >
-                      Tailwind CSS
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Follow us
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a
-                      href="https://github.com/themesberg/flowbite"
-                      className="hover:underline "
-                    >
-                      Github
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://discord.gg/4eeurUVvTy"
-                      className="hover:underline"
-                    >
-                      Discord
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Legal
-                </h2>
-                <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="hover:underline">
-                      Terms &amp; Conditions
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+    <footer className="mt-20 max-w-screen-xl mx-auto px-4 mb-10">
+      <div className="flex justify-between items-center">
+        <div>
+          <div>
+            <Image src={logo} alt="Logo" className="w-20" />
+          </div>
+          <p className="mt-6 max-w-lg text-[16px] text-[#4A5B5C]">
+            Certified Web 3.0 and Metaverse Developer A One and Quarter Years
+            Panaverse DAO Earn as you Learn Program Getting Ready for the Next
+            Generation of the Internet
+          </p>
+          <div className="flex gap-5 mt-4">
+            <FaFacebookF />
+            <FiYoutube />
+            <BsGithub />
+            <BsTwitter />
           </div>
         </div>
-      </footer>
+        <div>
+          <h2>Programs</h2>
+          <ul className="">
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Web 3.0 and Metaverse Developer
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Artificial Intelligence
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Cloud-Native Computing
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Ambient Computing and Iot
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Genomics and Bioformatics
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="/" className="text-[#4A5B5C]">
+                Network Programmability and Automation
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Pages</h2>
+          <ul>
+            <li className="mt-3 text-[#4A5B5C]">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="mt-3 text-[#4A5B5C]">
+              <Link href="/">Quarter 1</Link>
+            </li>
+            <li className="mt-3 text-[#4A5B5C]">
+              <Link href="/">Quarter 2</Link>
+            </li>
+            <li className="mt-3 text-[#4A5B5C]">
+              <Link href="/">Quarter 3</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
 };
 export default Footer;
